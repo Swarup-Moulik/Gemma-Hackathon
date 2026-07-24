26b29db6624468f9750a872e06bfae54a539be1e
Srutti <137700748+Srutti@users.noreply.github.com>
2026-07-24 20:47:53 +0530

    Add Chat route and update App component structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,18 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Landing from "./pages/Landing";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
 import Report from "./pages/Report";
+import Chat from "./pages/Chat";
 
 function App() {
+  const location = useLocation();
+  const isLanding = location.pathname === "/";
+
   return (
-    <div className="min-h-screen bg-background text-foreground flex flex-col">
+    <div className={isLanding ? "min-h-screen flex flex-col" : "min-h-screen bg-background text-foreground flex flex-col"}>
       {/* Global Navigation Header */}
       <Navbar />
 
@@ -19,6 +23,7 @@ function App() {
           <Route path="/scan" element={<Home />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/report" element={<Report />} />
+          <Route path="/chat" element={<Chat />} />
         </Routes>
       </main>
     </div>
@@ -26,3 +31,4 @@ function App() {
 }
 
 export default App;
+
